0c1387aa72484332839ae16c45a5a802fb6c95f4
Bun <mbunalski@gmail.com>
2021-03-15 14:48:07 -0400

    test pycharm

--- webscraping.py
+++ webscraping.py
@@ -4,3 +4,4 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path = "C:\\Users\\Bun\\Desktop\\College\\Python\\chromedriver.exe")
 driver.maximize_window()
 driver.get("https://www.google.com")
+print("hi")
